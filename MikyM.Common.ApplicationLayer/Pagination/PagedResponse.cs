52c5e3e758db6d2a2fb29c3c9ba9864c8353c240
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-05-10 20:50:26 +0200

    feat: pagination helpers

---
+++ MikyM.Common.ApplicationLayer/Pagination/PagedResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace MikyM.Common.ApplicationLayer.Pagination;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class PagedResponse<T> : Response<T>
+{
+    public PagedResponse(T data, int pageNumber, int pageSize, IEnumerable<string>? errors = null) : base(data,
+        errors)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+    }
+
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public Uri? FirstPage { get; set; }
+    public Uri? LastPage { get; set; }
+    public int TotalPages { get; set; }
+    public long TotalRecords { get; set; }
+    public Uri? NextPage { get; set; }
+    public Uri? PreviousPage { get; set; }
+}
---
+++ MikyM.Common.ApplicationLayer/Pagination/Response.cs
@@ -0,0 +1,32 @@
+﻿namespace MikyM.Common.ApplicationLayer.Pagination;
+
+public class Response
+{
+    public Response(IEnumerable<string>? errors) : this(null, errors)
+    {
+    }
+
+    public Response(string? message = null, IEnumerable<string>? errors = null)
+    {
+        Message = message ?? string.Empty;
+        Errors = errors?.ToArray();
+    }
+
+    public bool IsSuccess => Errors is null;
+    public string[]? Errors { get; set; }
+    public string? Message { get; set; }
+}
+
+public class Response<T> : Response
+{
+    public Response(T? data, IEnumerable<string>? errors) : this(data, null, errors)
+    {
+    }
+
+    public Response(T? data, string? message = null, IEnumerable<string>? errors = null) : base(message, errors)
+    {
+        Data = data;
+    }
+
+    public T? Data { get; set; }
+}
